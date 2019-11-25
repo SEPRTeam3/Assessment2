39874f6cc65f4b06d411523b5d6400a3d75a74ca
superev12 <evan.snoswell@gmail.com>
2019-11-25 11:49:34 +0000

    Integrated Tiled map rendering

---
+++ core/assets/CityProto.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="CityProto" tilewidth="32" tileheight="32" tilecount="644" columns="28">
+ <image source="city_extension.png" width="896" height="736"/>
+</tileset>
--- core/src/com/kroy/game/scenes/GameScreen.java
+++ core/src/com/kroy/game/scenes/GameScreen.java
@@ -3,8 +3,13 @@ package com.kroy.game.scenes;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.kroy.game.MyGdxGame;
 import com.kroy.game.entities.Entity.entityID;
@@ -23,23 +28,34 @@ public class GameScreen implements Screen
 	
 	final MyGdxGame game;
 	Map map;
+	
+	private OrthogonalTiledMapRenderer ter;
+	private TiledMap tileMap;
+	private OrthographicCamera camera;
 	Vector2 selected;
 	turnStates turnState;
+
 	
 	public GameScreen(final MyGdxGame game)
 	{
+		
 		this.game = game;
 		this.map = new Map();
 		this.selected = null;
 		this.turnState = turnStates.PLAYER;
 		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 512, 512);
+		tileMap = new TmxMapLoader().load("tilemap.tmx");
+		ter = new OrthogonalTiledMapRenderer(tileMap);
+		
+		
 		this.map.debugMakeBuilding(5, 5);
 	}
 
 	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
+	public void show() 
+	{
 	}
 
 	@Override
@@ -48,13 +64,18 @@ public class GameScreen implements Screen
 		// Render
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		Gdx.gl.glClearColor(1, 0, 1, 1);
+		
+		ter.setView(camera);
+		ter.render();
+		
 		game.batch.begin();
+		
 		for (int i = 0; i < Map.WIDTH; i++)
 		{
 			for (int j = 0; j < Map.HEIGHT; j++)
 			{
 				// Render background
-				game.batch.draw(map.getBackground(i, j).getTexture(), i*32, j*32, 32, 32);
+				//game.batch.draw(map.getBackground(i, j).getTexture(), i*32, j*32, 32, 32);
 				// Render entities
 				if (map.getEntity(i, j) != null)
 				{
@@ -88,7 +109,6 @@ public class GameScreen implements Screen
 				{
 					map.moveEntity((int)selected.x, (int)selected.y, tileX, tileY);
 				}
-				//map.debugMakeFiretruck(tileX, tileY);
 			}
 			
 			// Space key handling
@@ -144,6 +164,7 @@ public class GameScreen implements Screen
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+		ter.dispose();
+		this.tileMap.dispose();
 	}
 }
