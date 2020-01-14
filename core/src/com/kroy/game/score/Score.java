dc73921acc77ce041902c5330ce35025abdc7587
Billy <bm1008@york.ac.uk>
2020-01-14 00:55:47 +0000

    Score shouldn't break everything now

--- core/src/com/kroy/game/score/Score.java
+++ core/src/com/kroy/game/score/Score.java
@@ -4,21 +4,33 @@ public class Score {
 
     private String name;
     private Integer value;
+    private Integer ID;
 
-    public Score(String name, Integer value){
+    public Score(String name, Integer value, Integer ID){
         this.name = name;
         this.value = value;
+        this.ID = ID;
     }
     public Score()
     {
         this.value = 0;
         this.name = "Undefined_User";
+        this.ID = -1;
     }
     public String getName(){
         return this.name;
     }
     public Integer getValue(){
         return this.value;
+    }
+    public String getValuestr(){
+        return this.value.toString();
+    }
+    public Integer getIDint(){
+        return this.ID;
+    }
+    public String getIDstr(){
+        return this.ID.toString();
     }
     public void addScore(int value){
         this.value += value;
@@ -29,8 +41,11 @@ public class Score {
     public void subtractScore(int value) {
         this.value -= value;
     }
-    public void changeName(String name)
+    public void setName(String name)
     {
         this.name = name;
+    }
+    public void setID(Integer ID){
+        this.ID = ID;
     }
 }
