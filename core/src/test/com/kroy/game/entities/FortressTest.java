0f98467a0671af90540e8378d2fe8311d8575fa1
ds1410 <57757507+ds1410@users.noreply.github.com>
2020-01-15 12:42:25 +0000

    added setter and getter for fortress entity

--- core/src/test/com/kroy/game/entities/FortressTest.java
+++ core/src/test/com/kroy/game/entities/FortressTest.java
@@ -1,8 +1,37 @@
 package com.kroy.game.entities;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FortressTest {
 
 
+    @Test
+    void takeDamage() {
+        Fortress testfortress = new Fortress();
+        assertTrue(testfortress.takeDamage(5));
+        testfortress.setHealth(4);
+        assertTrue(testfortress.takeDamage(10));
+        testfortress.setHealth(4);
+        assertFalse(testfortress.takeDamage(0));
+        //assertFalse(testfortress.takeDamage(-2));
+        //assertFalse(testfortress.takeDamage(3));
+    }
+
+    @Test
+    void getAttackStrength() {
+    }
+
+    @Test
+    void getAttackRadius() {
+    }
+
+    @Test
+    void getTargetsPerTurn() {
+    }
+
+    @Test
+    void levelUp() {
+    }
 }
